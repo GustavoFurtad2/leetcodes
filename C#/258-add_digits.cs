8219e26d717a8abd896e9dac3365688043b3c332
Gustavofurtad2 <gustav0furt.fatality@gmail.com>
2025-11-07 15:19:09 -0300

    add 258-add_digits.cs leetcode

---
+++ C#/258-add_digits.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int AddDigits(int num) {
+        
+        if (num > -10 && num < 10) {
+            return num;
+        }
+
+        int addedDigitsNumber = 0;
+
+        do {
+
+            addedDigitsNumber = 0;
+
+            while (num != 0) {
+
+                addedDigitsNumber += num % 10;
+
+                num /= 10;
+            }
+
+            num = addedDigitsNumber;
+        } while (addedDigitsNumber < -9 || addedDigitsNumber > 9);
+
+        return addedDigitsNumber;
+    }
+}
